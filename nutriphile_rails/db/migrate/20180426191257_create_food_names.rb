d11e5b6bfb1aa34a14e168de36937712376e95ca
Rina Hong <rhong1006@gmail.com>
2018-04-26 12:14:17 -0700

    Store all csv files in public directory, generate food_name table

---
+++ nutriphile_rails/db/migrate/20180426191257_create_food_names.rb
@@ -0,0 +1,18 @@
+class CreateFoodNames < ActiveRecord::Migration[5.1]
+  def change
+    create_table :food_names do |t|
+      t.integer :food_id
+      t.integer, :food_code
+      t.integer :food_group_id
+      t.integer :food_source_id
+      t.string :food_description
+      t.string :food_description_f
+      t.date :food_date_of_entry
+      t.date :food_date_of_publication
+      t.integer :country_code
+      t.string :scientific_name
+
+      t.timestamps
+    end
+  end
+end
