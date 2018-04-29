f3007ad67499f307cc967ce9c7cbff7ee3db3abd
Rina Hong <rhong1006@gmail.com>
2018-04-29 13:08:17 -0700

    Set up full calendar

--- nutriphile_rails/app/assets/javascripts/application.js
+++ nutriphile_rails/app/assets/javascripts/application.js
@@ -10,5 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require moment
+//= require fullcalendar
 //= require rails-ujs
 //= require_tree .
