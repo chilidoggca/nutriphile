a11e625665a643a3d6bdf34e8561323d40bbdcb4
chilidoggca <jack.chili.lee@gmail.com>
2018-04-28 22:43:08 -0700

    installed devise

--- nutriphile_rails/db/schema.rb
+++ nutriphile_rails/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180427200459) do
+ActiveRecord::Schema.define(version: 20180429053552) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,23 @@ ActiveRecord::Schema.define(version: 20180427200459) do
     t.index ["nutrient_amount_id"], name: "index_nutrients_on_nutrient_amount_id"
     t.index ["nutrient_name_id"], name: "index_nutrients_on_nutrient_name_id"
     t.index ["nutrient_number"], name: "index_nutrients_on_nutrient_number"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "nutrients", "foods"
